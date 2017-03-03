372faa7c4874e87a157f56cd9eabd4ec72cca89f
raqlol <raquel.lowell@gmail.com>
2017-03-03 10:03:16 -0800

    cleaned up code

--- site.js
+++ site.js
@@ -1,12 +1,14 @@
 var menuIcon = document.getElementById("menu-icon");
 menuIcon.addEventListener("click", determinesState);
+
 function determinesState () {
   this.classList.toggle('open');
-  var opened = document.getElementById("dropdown").classList.contains('open-menu');
+  var dropdown = document.getElementById("dropdown")
+  var opened = dropdown.classList.contains('open-menu');
   if (opened) {
-    document.getElementById("dropdown").classList.remove('open-menu');
+    dropdown.classList.remove('open-menu');
   }
   else {
-    document.getElementById("dropdown").classList.add('open-menu');
+    dropdown.classList.add('open-menu');
   }
 }
